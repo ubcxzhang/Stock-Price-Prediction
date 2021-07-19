9507b26c5e58a8559ac7d44c6b4adcde6dd1a8d1
YuyingHUANG <44936202+YuyingHUANG@users.noreply.github.com>
2021-07-19 02:16:36 -0700

    ensemble results with SVM model
    implement 'ensemble_svm.R' through .sh file

---
+++ code/ensemble_svm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -J BV1
+#SBATCH --account=rrg-ubcxzh
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=40G
+#SBATCH -t 0-05:58:00
+
+module load nixpkgs/16.09 gcc/7.3.0 r/3.6.1
+
+Rscript --vanilla ~/projects/def-ubcxzh/y2huang/midprice_predict/final_version_2/ensemble_svm.R $1 > ~/projects/def-ubcxzh/y2huang/midprice_predict/final_version_2/result/ensemble_svm.$1.Rout 2>&1
+
+
+ 
