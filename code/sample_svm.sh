29035cb437bb845b50957ab66ef7a7f8d3bb2007
YuyingHUANG <44936202+YuyingHUANG@users.noreply.github.com>
2021-07-19 02:13:39 -0700

    experiments with SVM model
    implement of 'sample.R' through .sh file

---
+++ code/sample_svm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -J BV1
+#SBATCH --account=rrg-ubcxzh
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=40G
+#SBATCH -t 0-02:58:00
+
+
+module load nixpkgs/16.09 gcc/7.3.0 r/3.6.1
+
+
+Rscript --vanilla ~/projects/def-ubcxzh/y2huang/midprice_predict/final_version_2/sample.R $1 > ~/projects/def-ubcxzh/y2huang/midprice_predict/final_version_2/result/sample_svm.$1.Rout 2>&1
+
+
+ 
