5787e43f026bdf56e63a0e3e3cfab69598202e9d
YuyingHUANG <44936202+YuyingHUANG@users.noreply.github.com>
2021-07-26 20:44:12 -0700

    Update sample_svm.sh

--- code/sample_svm.sh
+++ code/sample_svm.sh
@@ -9,7 +9,7 @@
 module load nixpkgs/16.09 gcc/7.3.0 r/3.6.1
 
 
-Rscript --vanilla ~/projects/def-ubcxzh/y2huang/midprice_predict/final_version_2/sample.R $1 > ~/projects/def-ubcxzh/y2huang/midprice_predict/final_version_2/result/sample_svm.$1.Rout 2>&1
+Rscript --vanilla ~/projects/def-ubcxzh/y2huang/midprice_predict/thesis/sample.R $1 > ~/projects/def-ubcxzh/y2huang/midprice_predict/thesis/result/sample_svm.$1.Rout 2>&1
 
 
  
