605367969453a50a34c208436b70ee4f4b8e1c59
YuyingHUANG <44936202+YuyingHUANG@users.noreply.github.com>
2021-07-26 20:23:08 -0700

    Update ensemble_svm.sh

--- code/ensemble_svm.sh
+++ code/ensemble_svm.sh
@@ -7,7 +7,7 @@
 
 module load nixpkgs/16.09 gcc/7.3.0 r/3.6.1
 
-Rscript --vanilla ~/projects/def-ubcxzh/y2huang/midprice_predict/final_version_2/ensemble_svm.R $1 > ~/projects/def-ubcxzh/y2huang/midprice_predict/final_version_2/result/ensemble_svm.$1.Rout 2>&1
+Rscript --vanilla ~/projects/def-ubcxzh/y2huang/midprice_predict/thesis/ensemble_svm.R $1 > ~/projects/def-ubcxzh/y2huang/midprice_predict/thesis/result/ensemble_svm.$1.Rout 2>&1
 
 
  
