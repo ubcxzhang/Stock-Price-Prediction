8bf6c8f3da612fe40b25307fe9be5b9a76730f47
YuyingHUANG <44936202+YuyingHUANG@users.noreply.github.com>
2021-07-26 20:18:06 -0700

    Update feature_encoding.sh

--- code/feature_encoding.sh
+++ code/feature_encoding.sh
@@ -7,7 +7,7 @@
 
 module load nixpkgs/16.09 gcc/7.3.0 r/3.6.1
 
-Rscript --vanilla ~/projects/def-ubcxzh/y2huang/midprice_predict/final_version_2/feature_encoding.R > ~/projects/def-ubcxzh/y2huang/midprice_predict/final_version_2/result/feature_encoding.Rout 2>&1
+Rscript --vanilla ~/projects/def-ubcxzh/y2huang/midprice_predict/thesis/feature_encoding.R > ~/projects/def-ubcxzh/y2huang/midprice_predict/thesis/result/feature_encoding.Rout 2>&1
 
 
  
