f5f0df592eb47d10327c8f06f411fd3dc66d3999
YuyingHUANG <44936202+YuyingHUANG@users.noreply.github.com>
2021-07-26 20:50:28 -0700

    Update sample_ELN_full.sh

--- code/sample_ELN_full.sh
+++ code/sample_ELN_full.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -J BV1
-#SBATCH --account=rrg-ubcxzh
+#SBATCH --account=def-ubcxzh
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=50G
 #SBATCH -t 0-2:59:00
@@ -9,7 +9,7 @@
 module load nixpkgs/16.09 gcc/7.3.0 r/3.6.1
 
 
-Rscript --vanilla ~/projects/def-ubcxzh/y2huang/midprice_predict/final_version_2/sample_ELN_full.R $1 > ~/projects/def-ubcxzh/y2huang/midprice_predict/final_version_2/result_ELN_nocut/sample_ELN_full.$1.Rout 2>&1
+Rscript --vanilla ~/projects/def-ubcxzh/y2huang/midprice_predict/thesis/sample_ELN_full.R $1 > ~/projects/def-ubcxzh/y2huang/midprice_predict/thesis/result/sample_ELN_full.$1.Rout 2>&1
 
 
  
