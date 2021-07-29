ddba014ffd4e33fa1dbb0690c7b1790dcc690672
YuyingHUANG <44936202+YuyingHUANG@users.noreply.github.com>
2021-07-28 19:10:36 -0700

    Add files via upload

---
+++ sh/data_cleaning.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -J BV1
+#SBATCH --account=def-ubcxzh
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=40G
+#SBATCH -t 0-20:00:00
+
+module load nixpkgs/16.09 
+module load gcc/7.3.0 
+module spider r/3.6.1
+module load r/3.6.1
+
+Rscript --vanilla ./code/data_cleaning.R > ./rout/data_cleaning.Rout 2>&1
+
+
+ 
