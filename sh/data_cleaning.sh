13d0b9f7a435db7f0acc03792ec822fd5d4f72da
YuyingHUANG <44936202+YuyingHUANG@users.noreply.github.com>
2021-07-28 20:09:06 -0700

    Update data_cleaning.sh

--- sh/data_cleaning.sh
+++ sh/data_cleaning.sh
@@ -7,8 +7,8 @@
 
 module load nixpkgs/16.09 
 module load gcc/7.3.0 
-module spider r/3.6.1
-module load r/3.6.1
+module spider r/4.0.2
+module load r/4.0.2
 
 Rscript --vanilla ./code/data_cleaning.R > ./rout/data_cleaning.Rout 2>&1
 
