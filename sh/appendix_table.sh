ad4b6133ffe519616eab1d803fe52b4aa85e4753
YuyingHUANG <44936202+YuyingHUANG@users.noreply.github.com>
2021-08-01 15:37:03 -0700

    Add files via upload

---
+++ sh/appendix_table.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -J BV1
+#SBATCH --account=def-ubcxzh
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=50G
+#SBATCH -t 0-04:00:00
+
+module load nixpkgs/16.09 
+module load gcc/7.3.0 
+module spider r/4.0.2
+module load r/4.0.2
+
+Rscript --vanilla ./code/appendix_table.R > ./rout/appendix_table.Rout 2>&1
+
+
+ 
